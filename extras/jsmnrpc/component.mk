7e8dbc2d16d1d1fe76b8cfe774bad2375c871314
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-06-22 21:49:53 +0200

    Correct component.mk
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- extras/jsmnrpc/component.mk
+++ extras/jsmnrpc/component.mk
@@ -4,7 +4,7 @@
 INC_DIRS += $(jsmnrpc_ROOT)jsmnrpc/inc
 
 # args for passing into compile rule generation
-jsmn_INC_DIR = $(jsmnrpc_ROOT)jsmnrpc/inc
-jsmn_SRC_DIR = $(jsmnrpc_ROOT)jsmnrpc/src
+jsmnrpc_INC_DIR = $(jsmnrpc_ROOT)jsmnrpc/inc
+jsmnrpc_SRC_DIR = $(jsmnrpc_ROOT)jsmnrpc/src
 
 $(eval $(call component_compile_rules,jsmnrpc))
